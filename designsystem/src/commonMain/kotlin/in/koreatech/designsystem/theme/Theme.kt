c4ddaa67b49da02a7dcc7a6dd8eb064591b26578
WooJin Kong <kongwoojin03@gmail.com>
2025-10-06 03:07:33 +0900

    add: Add Colors

--- designsystem/src/commonMain/kotlin/in/koreatech/designsystem/theme/Theme.kt
+++ designsystem/src/commonMain/kotlin/in/koreatech/designsystem/theme/Theme.kt
@@ -1,14 +1,17 @@
 package `in`.koreatech.designsystem.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 
 @Composable
 fun KoinTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     CompositionLocalProvider(
-        LocalTypography provides provideKoinTypography()
+        LocalTypography provides provideKoinTypography(),
+        LocalColors provides lightColors // TODO: Add darkColors when dark theme defined
     ) {
         content()
     }
