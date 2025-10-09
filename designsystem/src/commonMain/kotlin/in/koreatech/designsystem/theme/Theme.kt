1f107dc2417a88a95f41dadb3fd13d6bdc474497
WooJin Kong <kongwoojin03@gmail.com>
2025-10-09 19:56:05 +0900

    add: Add KoinTheme colors and typography

--- designsystem/src/commonMain/kotlin/in/koreatech/designsystem/theme/Theme.kt
+++ designsystem/src/commonMain/kotlin/in/koreatech/designsystem/theme/Theme.kt
@@ -3,6 +3,8 @@ package `in`.koreatech.designsystem.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.text.TextStyle
 
 @Composable
 fun KoinTheme(
@@ -11,8 +13,20 @@ fun KoinTheme(
 ) {
     CompositionLocalProvider(
         LocalTypography provides provideKoinTypography(),
+        LocalTextStyle provides TextStyle.Default,
         LocalColors provides lightColors // TODO: Add darkColors when dark theme defined
     ) {
         content()
     }
+}
+
+object KoinTheme {
+    val colors: KoinColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColors.current
+    val typography: KoinTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
 }
